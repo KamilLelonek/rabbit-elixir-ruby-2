ad20c2b00de30357632bdaa7cf37a565fb18ec44
Kamil Lelonek <squixy.sln@gmail.com>
2016-11-14 07:56:28 +0100

    Use extracted modules

--- rabbit_elixir/lib/rabbit_elixir/event_receiver.ex
+++ rabbit_elixir/lib/rabbit_elixir/event_receiver.ex
@@ -1,24 +1,20 @@
 defmodule RabbitElixir.EventReceiver do
-  @queue "rabbit_elixir_ruby_topic"
-  @topic System.get_env("RABBIT_ELIXIR_RUBY_TOPIC") || "RABBIT_ELIXIR_RUBY_TOPIC"
+  import RabbitElixir
+
+  alias RabbitElixir.ConnManager
 
   def start_listening() do
-    with {:ok, channel} <- channel() do
+    with {:ok, channel} <- ConnManager.channel() do
       prepare(channel)
       wait_for_messages(channel)
     end
   end
 
-  defp channel() do
-    with {:ok, connection} <- AMQP.Connection.open(),
-      do: AMQP.Channel.open(connection)
-  end
-
   defp prepare(channel) do
-    AMQP.Exchange.declare(channel, @topic, :topic)
-    AMQP.Queue.declare(channel, @queue, exclusive: true)
-    AMQP.Queue.bind(channel, @queue, @topic)
-    AMQP.Basic.consume(channel, @queue, nil, no_ack: true)
+    AMQP.Exchange.declare(channel, topic(), :topic)
+    AMQP.Queue.declare(channel, queue(), exclusive: true)
+    AMQP.Queue.bind(channel, queue(), topic())
+    AMQP.Basic.consume(channel, queue(), nil, no_ack: true)
   end
 
   def wait_for_messages(channel) do
@@ -32,6 +28,3 @@ defmodule RabbitElixir.EventReceiver do
   defp log_received(payload),
     do: IO.puts "[x] Received #{inspect Poison.Parser.parse!(payload)}"
 end
-
-
-

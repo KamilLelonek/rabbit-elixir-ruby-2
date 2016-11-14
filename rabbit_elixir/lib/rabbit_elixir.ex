6280f75e3831ba6c92b926af8427413bee71d8e8
Kamil Lelonek <squixy.sln@gmail.com>
2016-11-14 07:56:00 +0100

    Extract queue and topic names to top module

--- rabbit_elixir/lib/rabbit_elixir.ex
+++ rabbit_elixir/lib/rabbit_elixir.ex
@@ -1,2 +1,7 @@
 defmodule RabbitElixir do
+  def queue(),
+    do: "rabbit_elixir_ruby_topic"
+
+  def topic(),
+    do: System.get_env("RABBIT_ELIXIR_RUBY_TOPIC") || "RABBIT_ELIXIR_RUBY_TOPIC"
 end
